8ecf61dd31cc68d08665f153ec4a993add81eb22
Koichi Sasada <ko1@atdot.net>
2020-07-20 11:15:45 +0900

    WIP: fix more

--- common.mk
+++ common.mk
@@ -3389,6 +3389,7 @@ cont.$(OBJEXT): {$(VPATH)}method.h
 cont.$(OBJEXT): {$(VPATH)}missing.h
 cont.$(OBJEXT): {$(VPATH)}mjit.h
 cont.$(OBJEXT): {$(VPATH)}node.h
+cont.$(OBJEXT): {$(VPATH)}ractor.h
 cont.$(OBJEXT): {$(VPATH)}ruby_assert.h
 cont.$(OBJEXT): {$(VPATH)}ruby_atomic.h
 cont.$(OBJEXT): {$(VPATH)}st.h
